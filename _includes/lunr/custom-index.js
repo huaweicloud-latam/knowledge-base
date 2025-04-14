3685f91cbf8ef8922716adb61df2ef1f39927e23
Diogo Hatz <124005927+Hatz-D@users.noreply.github.com>
2025-04-14 14:03:00 -0300

    Update custom-index.js

--- _includes/lunr/custom-index.js
+++ _includes/lunr/custom-index.js
@@ -2,6 +2,5 @@ const content_to_merge = [docs[i].content, docs[i].lang];
 docs[i].content = content_to_merge.join(' ');
 
 var currentLang = '{{ site.lang }}';
-console.log("Lingua:");
 console.log(currentLang);
 //if (docs[i].lang != currentLang) {continue;}
